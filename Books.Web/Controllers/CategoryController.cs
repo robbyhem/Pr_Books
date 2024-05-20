cf723352f63162f11e957d2554e9863cbe7f2164
Chisom Iheme <chisompro@gmail.com>
2024-05-20 08:55:40 +0100

    Added and Implemented the Edit (Get and Post) method for Category Controller, and a view for the Edit methods - for updating categories

--- Books.Web/Controllers/CategoryController.cs
+++ Books.Web/Controllers/CategoryController.cs
@@ -12,16 +12,20 @@ namespace Books.Web.Controllers
         {
             _context = context;
         }
+
+        //Define a GET Action method for retrieving all records
         public IActionResult Index()
         {
             IEnumerable<Category> categories = _context.Categories;
             return View(categories);
         }
 
+        //Define the Create GET Action method
         public IActionResult Create()
         {
             return View();
         }
+        //Define the Create POST Action method
         [HttpPost]
         public IActionResult Create(Category category)
         {
@@ -32,6 +36,34 @@ namespace Books.Web.Controllers
             if (ModelState.IsValid)
             {
                 _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Define the Edit GET Action method
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category category = _context.Categories.First(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        //Define the Edit POST Action method
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
                 _context.SaveChanges();
                 return RedirectToAction("Index");
             }
