160ba851e3ca4ce2a8a5190038ddf371dc7bc7b2
Chisom Iheme <chisompro@gmail.com>
2024-05-29 10:55:07 +0100

    Created a single page for Create and Edit for Book model

--- Books.Web/Areas/Admin/Controllers/BookController.cs
+++ Books.Web/Areas/Admin/Controllers/BookController.cs
@@ -24,7 +24,7 @@ namespace Books.Web.Areas.Admin.Controllers
         }
 
         //Define the Create GET Action method
-        public IActionResult Create()
+        public IActionResult Upsert(int id)
         {
             BookVM bookVM = new BookVM()
             {
@@ -35,11 +35,22 @@ namespace Books.Web.Areas.Admin.Controllers
                 }),
                 Book = new Book()
             };
-            return View(bookVM);
+
+            if (id == null || id == 0)
+            {
+                //Create new record
+                return View(bookVM);
+            }
+            else
+            {
+                //Update existing record
+                bookVM.Book = _unitOfWork.Book.Get(x => x.Id == id);
+                return View(bookVM);
+            }
         }
         //Define the Create POST Action method
         [HttpPost]
-        public IActionResult Create(BookVM bookVM)
+        public IActionResult Upsert(BookVM bookVM, IFormFile file)
         {
             if (ModelState.IsValid)
             {
@@ -57,35 +68,6 @@ namespace Books.Web.Areas.Admin.Controllers
                 });
                 return View(bookVM);
             }
-        }
-
-        //Define the Edit GET Action method
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            Book book = _unitOfWork.Book.Get(x => x.Id == id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-            return View(book);
-        }
-        //Define the Edit POST Action method
-        [HttpPost]
-        public IActionResult Edit(Book book)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Book.Update(book);
-                _unitOfWork.Save();
-                TempData["success"] = "Book Updated Successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
         }
 
         //Define the Delete GET Action method
